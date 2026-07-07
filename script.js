340fc11f2da2cc7324e6e30a7f751d0ad55bac83
rahul <rahulravi456@gmail.com>
2026-07-07 23:38:59 +0530

    added first JS script file

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Script loaded successfully!");
+
+const startButton = document.getElementById("start-quiz-btn");
+console.log(startButton);
+
+startButton.addEventListener("click", function() {
+    console.log("Start Quiz button clicked!");
+    // Add your quiz starting logic here
+}); 
