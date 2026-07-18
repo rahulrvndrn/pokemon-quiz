d3e3800fb3b328fe34fa4f98273480a0f1b26a4b
rahul <rahulravi456@gmail.com>
2026-07-19 03:21:41 +0530

    added generations filters

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ const scoreDisplay = document.getElementById("score-display");
 
 const nextPokemonButton = document.getElementById("next-pokemon-btn");
 
+const generationRanges = {
+  "1": [1, 151],
+  "2": [152, 251],
+  "3": [252, 386],
+  "4": [387, 493],
+  "5": [494, 649],
+  "6": [650, 721],
+  "7": [722, 809],
+  "8": [810, 905],
+  "9": [906, 1025],
+  "10": [1, 1025]
+};
+
+const generationSelect = document.getElementById("generation-select");
 
 let correctAnswer = ""; // Example correct answer
 let score = 0; // Initialize score
@@ -32,10 +46,15 @@ function startNewRound() {
     console.log("Start Quiz button clicked!");
     guessInput.disabled = false; // Enable the input field for a new guess
     submitButton.disabled = false;
-    // Add your quiz starting logic here
-    const randomPokemonId = Math.floor(Math.random() * 1024) + 1; // Generate a random Pokemon ID between 1 and 1024
-   
     
+    // Add your quiz starting logic here
+    const selectedValue = generationSelect.value;
+    const range = generationRanges[selectedValue]; // e.g. [1, 151]
+    const min = range[0]; // first item in the array
+    const max = range[1]; // second item in the array
+
+    const randomPokemonId = Math.floor(Math.random() * (max - min + 1)) + min; // Generate a random Pokemon ID within the selected generation
+
     fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}`)
   .then(response => response.json())
   .then(data => {
